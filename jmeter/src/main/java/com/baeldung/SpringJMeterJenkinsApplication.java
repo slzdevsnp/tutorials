551c430c6ec74ee15f4fc117abd6a1caa8dfee54
sviatoslav zimine <slzdev@gmail.com>
2021-02-02 14:16:23 +0100

    jmeter module szi updates

--- jmeter/src/main/java/com/baeldung/SpringJMeterJenkinsApplication.java
+++ jmeter/src/main/java/com/baeldung/SpringJMeterJenkinsApplication.java
@@ -2,9 +2,11 @@ package com.baeldung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+//@Import({ApplicationConfig.class})
 @EnableMongoRepositories
 public class SpringJMeterJenkinsApplication {
 	public static void main(String[] args) {
